2ba3812e1d3832e249a8cc19b7d0c26f326d4801
Tommy Lee <leedox@naver.com>
2026-07-03 18:44:22 +0900

    커뮤니티 기본 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+  resources :comments, only: [:create, :destroy]
   devise_for :users
   # get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_02_141123) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_03_092041) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "commentable_id", null: false
+    t.string "commentable_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +42,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_02_141123) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
